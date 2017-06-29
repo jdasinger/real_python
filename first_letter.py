8105f0a01b02716e15202b34bc349c174587e3c1
James Dasinger <jamesdasinger@Jamess-MacBook-Pro-2.local>
2017-06-29 19:48:06 -0400

    first commit

---
+++ first_letter.py
@@ -0,0 +1,3 @@
+user_input = raw_input("Tell me your password: " )
+
+print user_input[0].upper()
---
+++ inputtest.py
@@ -0,0 +1,5 @@
+search_string = raw_input("Enter string to search: ")
+search_term = raw_input("Enter item to search for: ")
+
+print search_string.find(search_term)
+
