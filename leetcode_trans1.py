ed3bc1aa00da18a84a8aa7b2bf57288f6080e0d0
James Dasinger <jamesdasinger@Jamess-MBP-2.fios-router.home>
2018-10-11 18:50:47 -0400

    leet translators

---
+++ leetcode_trans1.py
@@ -0,0 +1,10 @@
+input_text = raw_input("Enter some text: ")
+
+leetcode = input_text.replace("a","4"). \ 
+                      replace("b","8"). \
+                      replace("e","3"). \
+                      replace("l","1"). \
+                      replace("o","0"). \
+                      replace("s","5"). \
+                      replace("t","7")
+print(leetcode)
---
+++ leetcode_trans2.py
@@ -0,0 +1,18 @@
+input_text = raw_input("Enter some text: ")
+
+leetreplace = {
+	"a": "4",
+	"b": "8",
+	"e": "3",
+	"l": "1",
+	"o": "0",
+	"s": "5",
+	"t": "7"
+}
+
+def leettrans(a, b):
+	for x,y in b.iteritems():
+		a = a.replace(x,y)
+	return a
+
+print leettrans(input_text,leetreplace)
